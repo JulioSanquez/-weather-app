a70a6ad7cd461f33ceb66013e730ba1664f628d2
Julio Sanquez <julio930911@gmail.com>
2022-11-28 00:36:30 -0600

    v1.1.1

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -13,7 +13,6 @@ const WeatherCard = ({weather, temperature, isCelsius, changeUnitTemperature, ne
         <h3>{weather.name}, {weather.sys.country}</h3>
         <section className='weatherCard-body'>
             <div className='img'>
-                {/* <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`} alt="Clima" /> */}
                 <img src={`http://localhost:5173/src/assets/${ clima ? clima.img : weather.weather[0].icon}.png`} alt="Clima" />
             </div>
             <div>
